78386884b638ff3fed1f14c46ab26423844d5e66
unknown <lynconlima00@gmail.com>
2024-10-11 13:28:01 -0300

    bug do ás resolvido

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import random
+import sys
+from time import sleep
 
 
 
 valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 naipes = ["Copas", "Ouros", "Espadas", "Paus"]
 
+def valida_res(msg):
+    while True:
+        r = str(input(msg)).strip().lower()[0]
+        if r in 'sn':
+            return r
+        else:
+            print('Informe uma opçao válida(tente (s)im ou (n)ão)!')
 class Carta:
     def __init__(self, valor, naipe):
         self.naipe = naipe
@@ -37,8 +46,10 @@ class Jogador:
         self.pontuacao = 0
 
     def ver_mao(self):
-        for carta in self.mao:
-            print(f'{carta}, ', end='')
+
+        for pos, carta in enumerate(self.mao):
+            print(f'{carta}', end='')
+            print(f', ' if pos < len(self.mao)-1 else '.', end='')
         print()
 
     def pedir_carta(self):
@@ -50,13 +61,21 @@ class Jogador:
 
     def calcular_pontucao(self):
         pontuacao_provisoria = 0
+        ases_na_mao = 0
+        n_ases_valendo_um = 0
         for carta in self.mao:
             if carta.valor.isnumeric():
                 pontuacao_provisoria += int(carta.valor)
             elif carta.valor in 'KQJ':
                     pontuacao_provisoria += 10
             else:
-                pontuacao_provisoria += 1
+                ases_na_mao += 1
+                pontuacao_provisoria += 11
+
+        while pontuacao_provisoria > 21 and ases_na_mao != n_ases_valendo_um:
+            n_ases_valendo_um += 1
+            pontuacao_provisoria -= 10
+
         self.pontuacao = pontuacao_provisoria
         return self.pontuacao
 
@@ -89,14 +108,14 @@ class Jogo:
         jogo.verificar_vencedor()
 
     def turno_do_jogador(self):
-        print('JOGADOR')
         while True:
             jogador.calcular_pontucao()
+            print(f'JOGADOR: {jogador.pontuacao}')
             jogador.ver_mao()
-            print(f'Pontuação: {jogador.pontuacao}')
             if jogador.verfificar_estouro():
                 break
-            res = str(input('Deseja mais uma carta?[s/n] ')).strip().lower()[0]
+            res = valida_res('Deseja mais uma carta?[s/n] ')
+            print()
             if res == 's':
                 jogador.pedir_carta()
             if res == 'n':
@@ -104,18 +123,18 @@ class Jogo:
 
     def turno_do_dealer(self):
         print()
-        print('DEALER')
         dealer.calcular_pontucao()
+        print(f'DEALER: {dealer.pontuacao}')
         dealer.ver_mao()
-        print(f'Pontuação: {dealer.pontuacao}')
+        sleep(1.5)
         print()
         if jogador.pontuacao <= 21:
             while dealer.pontuacao < jogador.pontuacao and dealer.pontuacao < 21:
                 dealer.pedir_carta()
-                print('DEALER')
                 dealer.calcular_pontucao()
+                print(f'DEALER: {dealer.pontuacao}')
                 dealer.ver_mao()
-                print(f'Pontuação: {dealer.pontuacao}')
+                sleep(1.5)
                 print()
 
     def verificar_vencedor(self):
@@ -126,8 +145,13 @@ class Jogo:
         else:
             print('Dealer venceu!')
 
-jogo = Jogo()
-jogador = Jogador()
-dealer = Jogador()
-jogo.jogadores.extend([jogador, dealer])
-jogo.inicializar_jogo()
+
+while True:
+    jogo = Jogo()
+    jogador = Jogador()
+    dealer = Jogador()
+    jogo.jogadores.extend([jogador, dealer])
+    jogo.inicializar_jogo()
+    jogar_novamente = valida_res('Jogar novamente? ')
+    if jogar_novamente == 'n':
+        sys.exit()
